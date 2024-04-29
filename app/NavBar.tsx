d854a44fba4430a7d5427aebc9071516b9db0f9c
perry-C <2558544545qwerty@gmail.com>
2024-04-29 14:01:46 +0100

    Feat: styled the active link in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
+
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
@@ -15,7 +22,11 @@ const NavBar = () => {
                 {links.map((link) => (
                     <Link
                         href={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true,
+                        })}
                     >
                         {link.label}
                     </Link>
