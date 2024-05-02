406bcd45bef3698d7dfebac345673d0be0cb938f
perry-C <2558544545qwerty@gmail.com>
2024-05-02 17:20:42 +0100

    Feat: form submission implemented

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,44 @@
 'use client';
 
-import React from 'react';
-import { TextField, Button } from '@radix-ui/themes';
-import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 
+import { Button, TextField } from '@radix-ui/themes';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+import React from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+type Inputs = { title: string; description: string };
+
 const NewIssuePage = () => {
+    const router = useRouter();
+
+    const { register, control, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        axios.post('/api/issues', data);
+        router.push('/issues');
+    };
     return (
-        <div className='max-w-xl space-y-3'>
-            <TextField.Root placeholder='Add new issue'></TextField.Root>
-            <SimpleMDE placeholder='Add issue description'></SimpleMDE>
+        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
+            <TextField.Root
+                placeholder='Add new issue'
+                {...register('title')}
+            ></TextField.Root>
+            <Controller
+                name='description'
+                control={control}
+                render={({ field }) => (
+                    <SimpleMDE
+                        placeholder='Add issue description'
+                        {...field}
+                    ></SimpleMDE>
+                )}
+            />
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     );
 };
 
