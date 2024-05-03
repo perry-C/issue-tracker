eabae1560428adf63df2f168ff0d736535375597
perry-C <2558544545qwerty@gmail.com>
2024-05-03 15:48:55 +0100

    Feat: client side error feedback

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { z } from 'zod';
 
 // "@" means the root of the project
 import prisma from '@/prisma/client';
+import { z } from 'zod';
 
 // used for validating the body of the request
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    title: z
+        .string({ required_error: 'A title for the issue is required' })
+        .min(1)
+        .max(255),
+    description: z
+        .string({
+            required_error: 'A description for the issue is required',
+            invalid_type_error: 'Description must be a string',
+        })
+        .min(1, 'Description is required'),
 });
 
 export async function POST(request: NextRequest) {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,10 +2,11 @@
 
 import 'easymde/dist/easymde.min.css';
 
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 
-import React from 'react';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 import SimpleMDE from 'react-simplemde-editor';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
@@ -16,29 +17,52 @@ const NewIssuePage = () => {
     const router = useRouter();
 
     const { register, control, handleSubmit } = useForm<Inputs>();
+    const [error, setError] = useState<any>();
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setError(error);
+        }
+    };
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => {
-        axios.post('/api/issues', data);
-        router.push('/issues');
+    const ErrorMessages = () => {
+        return error.response.data.map((res: any) => {
+            return (
+                <Callout.Root color='red' role='alert'>
+                    <Callout.Icon>
+                        <ExclamationTriangleIcon />
+                    </Callout.Icon>
+                    <Callout.Text>{res.message}</Callout.Text>
+                </Callout.Root>
+            );
+        });
     };
     return (
-        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
-            <TextField.Root
-                placeholder='Add new issue'
-                {...register('title')}
-            ></TextField.Root>
-            <Controller
-                name='description'
-                control={control}
-                render={({ field }) => (
-                    <SimpleMDE
-                        placeholder='Add issue description'
-                        {...field}
-                    ></SimpleMDE>
-                )}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
+        <div>
+            <form
+                className='max-w-xl space-y-3'
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <ErrorMessages></ErrorMessages>
+                <TextField.Root
+                    placeholder='Add new issue'
+                    {...register('title')}
+                ></TextField.Root>
+                <Controller
+                    name='description'
+                    control={control}
+                    render={({ field }) => (
+                        <SimpleMDE
+                            placeholder='Add issue description'
+                            {...field}
+                        ></SimpleMDE>
+                    )}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     );
 };
 
